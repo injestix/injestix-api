0273a5175c8f2e14959633f81f54adace3df7d92
Derek S. Rose <derek.s.rose21@gmail.com>
2015-10-12 20:41:21 -0700

    added base, storage, and services assemblies

--- Injestix.Gateway/Program.cs
+++ Injestix.Gateway/Program.cs
@@ -7,7 +7,7 @@ namespace Injestix.Gateway
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Injestix");
+            
 
             Thread.Sleep(Timeout.Infinite);
         }
