988a4d281b4d48301582134165581a667b36d31c
Eric Irwin <Eric.Irwin1124@gmail.com>
2015-10-12 18:13:22 -0700

    initial commit

---
+++ Injestix.Gateway/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace Injestix.Gateway
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Injestix");
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
